d8debb7df722e83de6d233163d62c8f869bec211
Frans Jansen <info@fransjansen.eu>
2021-09-07 11:19:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let naam = "Frans Jansen"
+let leeftijd = "42"; 
+let getal1 = 8;
+let getal2 = 3;
+
+console.log("Hello Winc Academy")
+console.log(naam)
+console.log(getal1 + getal2)
+console.log(getal1 - getal2)
+console.log(getal1 * getal2)
+console.log(getal1 / getal2)
+console.log(getal1 % getal2)
+console.log(leeftijd)
